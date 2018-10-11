6f8aec4bb756c47389453e128e27e4875e2ad836
Su Ann Ling <lingsuann02@gmail.com>
2018-10-11 13:18:50 +0200

    Add tests for asynchronous and synchronous post.

--- tests/phonect/SOAP/ClientTest.php
+++ tests/phonect/SOAP/ClientTest.php
@@ -10,75 +10,87 @@ use GuzzleHttp\Exception\RequestException;
 
 class ClientTest extends TestCase
 {
-	public function testCreateInstance()
-	{
-		$params = [
-		"id" => 123,
-		'file' => [
-			'content'  => 'content',
-			'filename' => 'name'
-			
-			]
-		];
-		$client = Client::createInstance(
-				'https://www.example.com/phonect-api/PhonectAPIService?wsdl',
-				'http://service.phonect.no/',
-				__DIR__ . '/../../../api-consumer.log' //Here log entries are stored under laravels storage path. 
-		);
+	private $client;
 
-		$this->assertInstanceOf(Client::class, $client);
-	}
-
-	public function testSoapSerialize() {
-		$_SERVER['REQUEST_URI'] = 'https://www.example.com/phonect-api/PhonectAPIService?wsdl';
-		$client = Client::createInstance(
+	public function __construct() {
+		$this->client = Client::createInstance(
 			'https://www.example.com/phonect-api/PhonectAPIService?wsdl',
 			'http://service.phonect.no/',
 			'/tmp/' . md5(uniqid(rand(), true)) . 'api-consumer.log' //Here log entries are stored under laravels storage path. 
 		);
-		$body1 = \GuzzleHttp\Stream\Stream::factory('');
-		$body2 = \GuzzleHttp\Stream\Stream::factory('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-    <soap:Body>
-        <ns2:addPersonResponse xmlns:ns2="http://service.phonect.no/">
-            <return>335</return>
-        </ns2:addPersonResponse>
-    </soap:Body>
-</soap:Envelope>');
+
+		$_SERVER['REQUEST_URI'] = 'https://www.example.com/phonect-api/PhonectAPIService?wsdl';
+		$body = \GuzzleHttp\Stream\Stream::factory(
+		'<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+			<soap:Body>
+				<ns2:addPersonResponse xmlns:ns2="http://service.phonect.no/">
+					<return>335</return>
+				</ns2:addPersonResponse>
+			</soap:Body>
+		</soap:Envelope>');
 
 		$mock = new MockHandler([
-			new Response(200, ['Content-Type' => 'text/xml; charset="UTF-8'], $body1),
-		    new Response(200, ['Content-Type' => 'text/xml; charset="UTF-8'], $body2)
+			new Response(200, ['Content-Type' => 'text/xml; charset="UTF-8'], $body)
 		]);
 
-
-		$config = $client->getClient()->getConfig();
+		$config = $this->client->getClient()->getConfig();
 		$handler = $config['handler'];
 		$handler->setHandler($mock);
-		$response = $client->getClient()->request('POST', 'https://www.example.com/phonect-api/PhonectAPIService?wsdl');
-		$this->assertEquals(200, $response->getStatusCode());
-		$response2 = null;
-		$promise = $client->addPerson(
-			['person' => ['firstName' => 'Phonect', 'lastName' => 'Nissen', 'iCustomer' => 123]]
-		)->then(function ($response) use (&$response2) { //someSoapAction == SOAPAction
-			$response2 = $response;
-	    }, function ($exception) {
-			throw $exception;
-	    });
-    	$promise->wait();
-		$this->assertEquals(200, $response2->getStatusCode());
-		$this->assertEquals(
-			'<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-    <soap:Body>
-        <ns2:addPersonResponse xmlns:ns2="http://service.phonect.no/">
-            <return>335</return>
-        </ns2:addPersonResponse>
-    </soap:Body>
-</soap:Envelope>',
-			(string)$response2->getBody());
-		$this->assertEquals((object)array('return' => 335), $response2->getBody()->soapSerialize());
 	}
 
-	public function testPost() {
-		@$this->assertEquals(null, Client::post());
+	public function createInstance() {
+		return Client::createInstance(
+			'https://www.example.com/phonect-api/PhonectAPIService?wsdl',
+			'http://service.phonect.no/',
+			'/tmp/' . md5(uniqid(rand(), true)) . 'api-consumer.log' //Here log entries are stored under laravels storage path. 
+		);
+	}
+
+	public function testThatSoapServiceIsRunning() {
+		$response = $this->client->getClient()->request('POST', 'https://www.example.com/phonect-api/PhonectAPIService?wsdl');
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testSyncPost() {
+		$result = null;
+		$result = $this->client->postSync(
+			'addPerson',
+			['person' => ['firstName' => 'Phonect', 'lastName' => 'Nissen', 'iCustomer' => 123]]
+		);
+		$this->assertEquals(200, $result->getStatusCode());
+		$this->assertEquals(
+		'<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+			<soap:Body>
+				<ns2:addPersonResponse xmlns:ns2="http://service.phonect.no/">
+					<return>335</return>
+				</ns2:addPersonResponse>
+			</soap:Body>
+		</soap:Envelope>',
+			(string) $result->getBody());
+		$this->assertEquals((object)array('return' => 335), $result->getBody()->soapSerialize());
+	}
+
+	public function testAsyncPost() {
+		$result = null;
+		$promise = $this->client->postAsync(
+			'addPerson',
+			['person' => ['firstName' => 'Phonect', 'lastName' => 'Nissen', 'iCustomer' => 123]]
+		)->then(function ($response) use (&$result){ //someSoapAction == SOAPAction
+			$result = $response;
+	    }, function ($exception) {
+			throw $exception;
+	    });;
+		$promise->wait();
+		$this->assertEquals(200, $result->getStatusCode());
+		$this->assertEquals(
+		'<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+			<soap:Body>
+				<ns2:addPersonResponse xmlns:ns2="http://service.phonect.no/">
+					<return>335</return>
+				</ns2:addPersonResponse>
+			</soap:Body>
+		</soap:Envelope>',
+			(string) $result->getBody());
+		$this->assertEquals((object)array('return' => 335), $result->getBody()->soapSerialize());
 	}
 }
